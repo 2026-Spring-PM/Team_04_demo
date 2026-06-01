8f4fd1e91eb387d694afae88c387b026581f2a29
limpid <limpidmarble@snu.ac.kr>
2026-06-01 23:03:20 +0900

    데모

---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+xhost +local:docker
+docker run -it --rm --platform linux/amd64 \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v $(pwd):/workspace \
+  jeongwanmoon/team_04_project:0.1.2 \
+  /workspace/build/main
